f66eb335c28b82ba74b34a8d9c3798f3d3a24174
disne_y <bold.disney@gmail.com>
2017-04-02 14:27:18 +0300

    Add relations between user and answer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :questions, dependent: :nullify
+  has_many :answers, dependent: :nullify
+
+  def author_of?(record)
+    record.user_id = id
+  end
 end
