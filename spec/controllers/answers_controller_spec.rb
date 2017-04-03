34a6a035872025ef76b9db1db0f299c7668812dd
disne_y <bold.disney@gmail.com>
2017-04-03 14:44:02 +0300

    Update tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
-  let(:answer) { create(:answer, question: question) }
+  let!(:answer) { create(:answer, question: question) }
+  sign_in_user
+  let!(:users_answer) { create(:answer, user: @user) }
 
   describe 'POST #create' do
     sign_in_user
@@ -20,37 +22,97 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with invalid attributes' do
       it 'does not save answer to db' do
-        expect { process :create, method: :post, params: { answer: attributes_for(:invalid_answer), question_id: question },
+        expect { process :create, method: :post,
+                         params: { answer: attributes_for(:invalid_answer), question_id: question },
                          format: :js }.to_not change(Answer, :count)
       end
 
       it 'renders create template with error messages' do
-        process :create, method: :post, params: { answer: attributes_for(:invalid_answer), question_id: question }, format: :js
+        process :create, method: :post, params: { answer: attributes_for(:invalid_answer), question_id: question },
+                format: :js
         expect(response).to render_template :create
       end
     end
   end
 
+  describe 'GET #edit' do
+    context 'edit by author' do
+      before { get :edit, xhr: true, params: { id: users_answer.id }, format: :js }
+
+      it 'assigns requested answer to @answer' do
+        expect(assigns(:answer)).to eq(users_answer)
+      end
+
+      it 'renders edit template' do
+        expect(response).to render_template :edit
+      end
+    end
+
+    context 'edit by someone else' do
+      before { get :edit, xhr: true, params: { id: answer.id }, format: :js }
+
+      it 'redirects to related question page' do
+        expect(response).to redirect_to question
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'update by author with valid attributes' do
+      before { process :update, method: :patch, params: { id: users_answer.id, answer: { body: 'new body' } },
+                       format: :js }
+
+      it 'assigns requested answer to @answer' do
+        expect(assigns(:answer)).to eq(users_answer)
+      end
+
+      it 'saves new answer to db' do
+        users_answer.reload
+        expect(users_answer.body).to eq 'new body'
+      end
+
+      it 'renders update template' do
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'update by author with invalid attributes' do
+      before { process :update, method: :patch, params: { id: users_answer.id, answer: { body: '' } },
+                       format: :js }
+
+      it 'does not save new answer to db' do
+        users_answer.reload
+        expect(users_answer.body).to match /^answer_body_\d+$/
+      end
+
+      it 'renders update template' do
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'update by someone else' do
+      it 'redirects to related question page' do
+        process :update, method: :patch, params: { id: answer.id, answer: attributes_for(:answer) }, format: :js
+        expect(response).to redirect_to question
+      end
+    end
+  end
+
+
   describe 'DELETE #destroy' do
-    sign_in_user
-  
     context 'delete by author' do
-      let!(:answer) { create(:answer, user: @user) }
-  
       it 'deletes answer from db' do
-        expect { process :destroy, method: :delete, params: { id: answer.id }, format: :js }
+        expect { process :destroy, method: :delete, params: { id: users_answer.id }, format: :js }
             .to change(Answer, :count).by(-1)
       end
 
       it 'renders destroy template' do
-        process :destroy, method: :delete, params: { id: answer.id }, format: :js
+        process :destroy, method: :delete, params: { id: users_answer.id }, format: :js
         expect(response).to render_template :destroy
       end
     end
 
     context 'delete by someone else' do
-      before { answer }
-
       it 'does not delete answer from db' do
         expect { process :destroy, method: :delete, params: { id: answer.id }, format: :js }
             .to_not change(Answer, :count)
@@ -61,6 +123,5 @@ RSpec.describe AnswersController, type: :controller do
         expect(response).to redirect_to question
       end
     end
-
-  end  
+  end
 end
