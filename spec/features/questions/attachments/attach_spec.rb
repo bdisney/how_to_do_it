1afea29c848aec9cda60c04556a8ba51112d8a31
disne_y <bold.disney@gmail.com>
2017-04-04 15:35:55 +0300

    Add attachment spec, update q&a specs

---
+++ spec/features/questions/attachments/attach_spec.rb
@@ -0,0 +1,25 @@
+require 'feature_spec_helper'
+
+feature 'Attach files to question', %q{
+  In order to give more information about my problem
+  As an author of a question
+  I want to attach files
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+      sign_in(user)
+      visit new_question_path
+    end
+
+  scenario 'Authenticated user creates question with attachments' do
+    data = attributes_for(:question)
+    fill_in 'Title', with: data[:title]
+    fill_in 'Body', with: data[:body]
+    attach_file 'File', "#{Rails.root}/spec/files/file_01.txt"
+    click_on 'Add question'
+
+    expect(page).to have_link 'file_01.txt', href: /uploads\/attachment\/file\/\d\/file_01\.txt/
+  end
+end
