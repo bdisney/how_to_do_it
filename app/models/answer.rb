24848698baaa12a6fd6c453839305de9456bf94e
disne_y <bold.disney@gmail.com>
2017-04-04 15:28:31 +0300

    Add relations with attachments to answer and question models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,9 +3,14 @@ class Answer < ApplicationRecord
 
   belongs_to :question
   belongs_to :user
+  has_many :attachments, dependent: :destroy, as: :attachable
 
   validates :body, presence: true, length: { minimum: 5 }
   validates :accepted, uniqueness: { scope: :question_id }, if: :accepted
+
+  accepts_nested_attributes_for :attachments,
+                                reject_if: proc { |attributes| attributes['file'].blank? },
+                                allow_destroy: true
 
   scope :accepted, -> { where(accepted: true) }
 
