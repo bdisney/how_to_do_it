de6b542f6ecfbd257d50b376977b23d97a334eef
disne_y <bold.disney@gmail.com>
2017-04-05 16:33:08 +0300

    Update attach specs

--- spec/features/questions/attachments/attach_spec.rb
+++ spec/features/questions/attachments/attach_spec.rb
@@ -13,13 +13,16 @@ feature 'Attach files to question', %q{
       visit new_question_path
     end
 
-  scenario 'Authenticated user creates question with attachments' do
+  scenario 'Authenticated user creates question with attachments', js: true do
     data = attributes_for(:question)
     fill_in 'Title', with: data[:title]
     fill_in 'Body', with: data[:body]
-    attach_file 'File', "#{Rails.root}/spec/files/file_01.txt"
+    first('input[type="file"]').set "#{Rails.root}/spec/files/file_01.txt"
+    click_on '+ Add file'
+    all('input[type="file"]').last.set "#{Rails.root}/spec/files/file_02.txt"
     click_on 'Add question'
 
     expect(page).to have_link 'file_01.txt', href: /uploads\/attachment\/file\/\d\/file_01\.txt/
+    expect(page).to have_link 'file_02.txt', href: /uploads\/attachment\/file\/\d\/file_02\.txt/
   end
 end
