681609180bdcdbe1bda791ac74e830981b482a67
disne_y <bold.disney@gmail.com>
2017-04-10 16:37:39 +0300

    Update QnA specs

--- spec/features/questions/create_spec.rb
+++ spec/features/questions/create_spec.rb
@@ -57,4 +57,32 @@ feature 'Create question', %q{
     expect(current_path).to eq new_user_session_path
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
+
+  scenario 'All users see new questions in real-time', js: true do
+    data = attributes_for(:question)
+
+    Capybara.using_session('author') do
+      sign_in(user)
+      visit questions_path
+    end
+
+    Capybara.using_session('guest') do
+      visit questions_path
+    end
+
+    Capybara.using_session('author') do
+      click_on 'Add question'
+
+      fill_in 'Title', with: data[:title]
+      fill_in 'Body', with: data[:body]
+      click_on 'Add question'
+
+      expect(page).to have_content data[:title]
+      expect(page).to have_content data[:body]
+    end
+
+    Capybara.using_session('guest') do
+      expect(page).to have_content data[:title]
+    end
+  end
 end
