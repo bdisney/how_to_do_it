cffc70077f12109f5d3e8f0526f66742a0942114
disne_y <bold.disney@gmail.com>
2017-04-12 15:55:50 +0300

    Add authorization via Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,26 @@ class User < ApplicationRecord
 
   has_many :questions, dependent: :nullify
   has_many :answers, dependent: :nullify
+  has_many :identities, dependent: :destroy
 
   def author_of?(record)
     record.user_id == id
+  end
+
+  def self.find_for_oauth(auth)
+    identity = Identity.find_or_initialize_by(provider: auth.provider, uid: auth.uid.to_s)
+    return identity.user if identity.persisted?
+    
+    user = User.find_or_initialize_by(email: auth.info[:email])
+    transaction do
+      unless user.persisted?
+        password = Devise::friendly_token(10)
+        user.assign_attributes(password: password, password_confirmation: password)
+        user.save!
+      end
+      identity.user = user
+      identity.save!
+    end
+    user
   end
 end
