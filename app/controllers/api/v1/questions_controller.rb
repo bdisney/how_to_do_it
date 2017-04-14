a37ad7590c24f2b57d6c44deefe6f81ab387b4b5
disne_y <bold.disney@gmail.com>
2017-04-14 16:15:57 +0300

    Update q api controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
   def index
-    @questions = Question.all
-    respond_with(@questions.to_json(include: :answers))
+    respond_with(@questions = Question.all)
   end
 end
