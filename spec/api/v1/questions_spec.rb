6078619e78c3807cb463a3bd24517f0e34b3559b
disne_y <bold.disney@gmail.com>
2017-04-14 16:16:10 +0300

    Update spec

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -27,23 +27,23 @@ describe 'Questions API' do
       end
 
       it 'returns questions list' do
-        expect(response.body).to have_json_size(2)
+        expect(response.body).to have_json_size(2).at_path('questions')
       end
 
       context 'each question object' do
         %w(id title body created_at updated_at).each do |attr|
           it "contains attribute #{attr}" do
-            expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+            expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("questions/0/#{attr}")
           end
         end
 
         it 'contains answers list' do
-          expect(response.body).to have_json_size(1).at_path('0/answers')
+          expect(response.body).to have_json_size(1).at_path('questions/0/answers')
         end
 
         %w(id body created_at updated_at).each do |attr|
           it "each answer contains attribute #{attr}" do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{attr}")
           end
         end
       end
