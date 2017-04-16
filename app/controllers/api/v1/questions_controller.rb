0b0263cc057e8a7916398114220c7f610ff5ddc7
disne_y <bold.disney@gmail.com>
2017-04-16 13:07:40 +0300

    Update questions api controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :get_question, only: [:show]
+
+  authorize_resource
+
   def index
     respond_with(@questions = Question.all)
+  end
+
+  def show
+    respond_with @question, serializer: SingleQuestionSerializer
+  end
+
+  private
+
+  def get_question
+    @question = Question.find(params[:id])
   end
 end
